44bc5c0bd38f07d23e03a703076dbe8ddb70e4f7
Juanse <pcjvan@gmail.com>
2022-08-02 19:35:50 -0500

    fix mock movie model fields types
    While parsing the mock movies json file, some types were not
    succesfully parsed in json.Deserializer method.

--- Models/Movie.cs
+++ Models/Movie.cs
@@ -2,7 +2,7 @@ namespace MovieFinder.Models;
 
 public class Movie
 {
-    public string? Popularity { get; set; }
+    public double Popularity { get; set; }
     public int VoteCount { get; set; }
     public bool Video { get; set; }
     public string? PosterPath { get; set; }
@@ -11,11 +11,10 @@ public class Movie
     public string? BackDropPath { get; set; }
     public string? OriginalLanguage { get; set; }
     public string? OriginalTitle { get; set; }
-    public Tuple<int>? GenreIds { get; set; }
+    public int[] GenreIds { get; set; }
     public string? Title { get; set; }
     public double VoteAverage { get; set; }
     public string? Overview { get; set; }
     public DateTime ReleaseDate { get; set; }
-    /* http://image.tmdb.org/t/p/w220_and_h330_face<PosterPath>*/
+    /* http://image.tmdb.org/t/p/w294_and_h330_face<PosterPath>*/
 }
-
